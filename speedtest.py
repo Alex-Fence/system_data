4fa45a7cb18ab23e5035f57c2f2d3460292d9370
Alex-Fence <56536699+Alex-Fence@users.noreply.github.com>
2024-10-30 09:54:28 +0300

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1,1 +1,6 @@
+from speedtest import Speedtest
+inet = Speedtest
+download = float(str(inet.download()) [0:2] + "." + str(round(inet.download(), 2)) [1]) * 0.125
+uploads = float(str(inet.upload()) [0:2] + "." + str(round(inet.download(), 2)) [1]) * 0.125
+print(f'{download:} {uploads:}')
 
